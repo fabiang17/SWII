40c66727917b5a76a5522fe756112d6661a2d9a7
Fabian <Fabian@GutirrezCorp>
2014-08-09 07:17:45 -0500

    Cargue de clases

---
+++ Marte/src/main/java/co/edu/unbosque/marte/Movimiento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.unbosque.marte;
+
+/**
+ *
+ * @author Fabian
+ */
+public class Movimiento {
+
+    public Movimiento(String iaaadiad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public char getNextMov() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
